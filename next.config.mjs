1fd056e966e0dcc796a7146275b7c98df0b319ec
andy <andersgee@gmail.com>
2023-10-19 03:28:31 +0200

    configured tailwind, font, validate-env and image-sizes

---
+++ next.config.mjs
@@ -0,0 +1,41 @@
+import "./src/utils/validate-process-env.mjs";
+
+//matching tailwind sizes:
+//                       sm   md    lg    xl    2xl   3xl...
+const DEVICE_SIZES = [640, 768, 1024, 1280, 1536, 2048];
+//  w-               6   8   12  16  24  32   64   96
+const IMAGE_SIZES = [24, 32, 48, 64, 96, 128, 256, 384];
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    imageSizes: IMAGE_SIZES,
+    deviceSizes: DEVICE_SIZES,
+    remotePatterns: [
+      /*
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.googleusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        pathname: "/some-bucket-name/**",
+      },
+      */
+    ],
+  },
+};
+
+export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,108 @@
-import type { Config } from 'tailwindcss'
+import { type Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
+import plugin from "tailwindcss/plugin";
+import { type ScreensConfig } from "tailwindcss/types/config";
 
-const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+//https://tailwindcss.com/docs/screens
+//make sure these values match what is in next.config.mjs
+const DEVICE_SIZES: ScreensConfig = {
+  sm: "640px",
+  md: "768px",
+  lg: "1024px",
+  xl: "1280px",
+  "2xl": "1536px",
+};
+
+export default {
+  content: ["./src/**/*.{ts,tsx}"],
   theme: {
+    screens: DEVICE_SIZES,
+    container: {
+      screens: DEVICE_SIZES,
+      center: true,
+    },
+
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+      boxShadow: {
+        //imageborder: "inset 0 2px 4px 0 hsla(0, 0%, 0%, .2)",
+        imageborder: "inset 0 0 0 1px hsla(0, 0%, 0%, .1)",
+      },
+      colors: {
+        sometest: "hsl(var(--sometest))",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [
+    require("tailwindcss-animate"),
+    plugin(({ addUtilities }) => {
+      addUtilities({
+        ".mainwidth": {
+          "@apply w-full sm:max-w-[436px] md:max-w-[520px] lg:max-w-[620px]":
+            {},
+        },
+        ".text-tweet": {
+          "@apply whitespace-pre-wrap break-words": {},
+        },
+        ".text-paragraph": {
+          "@apply max-w-[55ch] font-sans text-base font-normal leading-[1.55] tracking-[0.15px] text-neutral-600 [word-spacing:0.5px] dark:text-neutral-300":
+            {},
+        },
+      });
+    }),
+  ],
+} satisfies Config;
