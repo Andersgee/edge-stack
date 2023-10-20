4676a66ac26fffd27da3340805c589c9eba0800f
andy <andersgee@gmail.com>
2023-10-20 05:50:07 +0200

    sign in working

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useStore } from "#src/store";
+import { SigninButtons } from "./SigninButtons";
 
 type Props = {
   className?: string;
@@ -8,10 +9,12 @@ type Props = {
 
 export function Example({ className }: Props) {
   const user = useStore.select.user();
+
   return (
     <div className={className}>
       <div>Example</div>
       <div className="my-3 bg-slate-200 px-3">hello</div>
+      {user ? <div>signed in as {user.name}</div> : <SigninButtons />}
     </div>
   );
 }
