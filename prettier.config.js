f8d0c1fa72de29fade6bebf4cad205a900203557
andy <andersgee@gmail.com>
2023-10-20 02:31:27 +0200

    basic prettier.config

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  endOfLine: "lf",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  printWidth: 120,
+  trailingComma: "es5",
+};
