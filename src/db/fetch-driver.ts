d9680d9965c0a046a14788621292c8dc34c7396f
andy <andersgee@gmail.com>
2024-02-04 08:38:36 +0100

    encodeURIComponent

--- src/db/fetch-driver.ts
+++ src/db/fetch-driver.ts
@@ -77,7 +77,7 @@ class FetchConnection implements DatabaseConnection {
       parameters: compiledQuery.parameters,
     });
 
-    const url = `${this.config.url}?q=${q}`;
+    const url = `${this.config.url}?q=${encodeURIComponent(q)}`;
     const res = await fetch(url, this.config.init);
 
     if (res.ok) {
