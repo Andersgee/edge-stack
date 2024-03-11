058078d8177c85d262649588ba2bbf1edd1f287f
andy <andersgee@gmail.com>
2024-03-11 12:52:59 +0100

    import font directly in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,17 @@
 import "./globals.css";
-import { fontSans } from "#src/utils/font";
+import { Inter } from "next/font/google";
 import { apiRsc } from "#src/trpc/api-rsc";
 import { Providers } from "#src/context/Providers";
 import { seo } from "#src/utils/seo";
 import { TopnavLink } from "#src/components/TopnavLink";
 import { ProfileButton } from "#src/components/user/ProfileButton";
 import { Toaster } from "#src/ui/toaster";
+
+const fontSans = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-sans",
+});
 
 export const metadata = seo({
   title: "Boilerplate app",
